070993c704b8782bc620449cb5cfb84f4bf59d22
Hamik Mukelyan <hamik@caltech.edu>
2024-12-29 08:28:01 -0800

    error in arthmetic program... oops...

--- arithmetic_gen.py
+++ arithmetic_gen.py
@@ -11,7 +11,5 @@ num_lines_per_op = 15
 
 for op in ops:
     for i in range(num_lines_per_op):
-        rand_a = random.choice(a)
-        rand_b = random.choice(b)
-        print(f'{rand_a} {op} {rand_b} = \t\t\t\t{rand_a} {op} {rand_b} =')
+        print(f'{random.choice(a)} {op} {random.choice(a)} = \t\t\t\t{random.choice(a)} {op} {random.choice(a)} =')
     print()
