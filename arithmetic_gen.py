ea0175c2c480b35f811adc293d315da954be888e
Hamik Mukelyan <hamik@caltech.edu>
2025-01-23 16:30:14 -0800

    support for minus

--- arithmetic_gen.py
+++ arithmetic_gen.py
@@ -1,15 +1,21 @@
 import random
 
-max_num = 6
+max_num = 10
 
-a = [x for x in range(0, max_num + 1)]
-b = [x for x in range(0, max_num + 1)]
+nums = [x for x in range(0, max_num + 1)]
 
-ops = ['+']
+ops = ['-']
 
-num_lines_per_op = 15
+num_lines_per_op = 16
+
+def get_choices():
+    choice_a = random.choice(nums)
+    choice_b = random.choice(nums)
+    return (choice_a, choice_b) if op == '-'and choice_a > choice_b else (choice_b, choice_a)
 
 for op in ops:
     for i in range(num_lines_per_op):
-        print(f'{random.choice(a)} {op} {random.choice(a)} = \t\t\t\t{random.choice(a)} {op} {random.choice(a)} =')
+        choice_a, choice_b = get_choices()
+        choice_c, choice_d = get_choices()
+        print('%2d %2s %2d = \t\t%2d %2s %2d =' % (choice_a, op, choice_b, choice_c, op, choice_d))
     print()
