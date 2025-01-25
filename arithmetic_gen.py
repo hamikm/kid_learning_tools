e67b91f8c0b7297cacda6161b2ea094d015f4cb9
Hamik Mukelyan <hamik@caltech.edu>
2025-01-25 10:44:14 -0800

    made it easier to change num words to sample from in sight words program and added support for specifying operators in arithmetic practice on command line

--- arithmetic_gen.py
+++ arithmetic_gen.py
@@ -1,21 +1,28 @@
+import argparse
 import random
+
+parser = argparse.ArgumentParser(
+    prog='arithmetic helper',
+    description='produces a two column list of basic arithmetic practice problems')
+parser.add_argument('-o', '--operators', help='space delimited list of operators to use')
+args = parser.parse_args()
 
 max_num = 10
 
 nums = [x for x in range(0, max_num + 1)]
 
-ops = ['-']
+ops = args.operators.split(' ') if args.operators is not None else ['+']
 
 num_lines_per_op = 16
 
-def get_choices():
+def get_choices(is_subtraction=False):
     choice_a = random.choice(nums)
     choice_b = random.choice(nums)
-    return (choice_a, choice_b) if op == '-'and choice_a > choice_b else (choice_b, choice_a)
+    return (choice_a, choice_b) if is_subtraction and choice_a > choice_b else (choice_b, choice_a)
 
-for op in ops:
-    for i in range(num_lines_per_op):
-        choice_a, choice_b = get_choices()
-        choice_c, choice_d = get_choices()
-        print('%2d %2s %2d = \t\t%2d %2s %2d =' % (choice_a, op, choice_b, choice_c, op, choice_d))
-    print()
+for i in range(num_lines_per_op):
+    op1 = random.choice(ops)
+    choice_a, choice_b = get_choices(is_subtraction = op1 == '-')
+    op2 = random.choice(ops)
+    choice_c, choice_d = get_choices(is_subtraction = op1 == '-')
+    print('%2d %2s %2d = \t\t%2d %2s %2d =' % (choice_a, op1, choice_b, choice_c, op2, choice_d))
