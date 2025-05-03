e69f698898f885b16410eb562f1d09b457f9a938
Hamik Mukelyan <hamik@caltech.edu>
2025-05-03 13:03:40 -0700

    added generation of arithmetic problems with multiples of powers of 10

--- arithmetic_gen.py
+++ arithmetic_gen.py
@@ -6,12 +6,20 @@ parser = argparse.ArgumentParser(
     description='produces a two column list of basic arithmetic practice problems')
 parser.add_argument('-o', '--operators', help='space delimited list of operators to use')
 parser.add_argument('-m', '--max', help='biggest number to use')
+parser.add_argument('-x', '--multiple', help='multiples of... e.g. 10 or 100')
 args = parser.parse_args()
 
-nums = [x for x in range(0, (int(args.max) if args.max is not None else 10) + 1)]
+maxMultiple = int(args.multiple) if args.multiple is not None else 1
+startingNums = [x for x in range(0, (int(args.max) if args.max is not None else 10) + 1)]
 ops = args.operators.split(' ') if args.operators is not None else ['+']
 
-num_lines_per_op = 16
+multiple = maxMultiple
+nums = []
+while (multiple > 1):
+    nums.extend([x * multiple for x in startingNums])
+    multiple /= 10
+
+num_lines_per_op = 20
 
 def get_choices(is_subtraction=False):
     choice_a = random.choice(nums)
@@ -23,4 +31,4 @@ for i in range(num_lines_per_op):
     choice_a, choice_b = get_choices(is_subtraction = op1 == '-')
     op2 = random.choice(ops)
     choice_c, choice_d = get_choices(is_subtraction = op2 == '-')
-    print('%2d %2s %2d = \t\t%2d %2s %2d =' % (choice_a, op1, choice_b, choice_c, op2, choice_d))
+    print('%3d %2s %3d = \t  %3d %2s %3d =' % (choice_a, op1, choice_b, choice_c, op2, choice_d))
